26972c5a2fb89b5888e5c3ae2777709693aa4891
ssagar7007 <ssagargupta7007@gmail.com>
2023-05-23 16:31:31 +0530

    UI improved loader added

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -146,6 +146,7 @@ const HomePage = () => {
         <div className="col-md-9 offset-1" onScroll={()=> setPage(page + 1)}>
           <h1 className="text-center">All Products</h1>
           <div className="d-flex flex-wrap">
+          <div> {loading?<img src={loader_img} alt="Loading..."/>:"" }</div>
             {products?.map((p) => (
               <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
                 <img
